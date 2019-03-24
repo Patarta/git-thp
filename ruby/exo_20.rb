5b6f8742024b20d91651c0c146989669caffbf58
phommasone <thibaultphommasone@gmail.com>
2019-03-24 09:47:01 +0100

    changement

--- ruby/exo_20.rb
+++ ruby/exo_20.rb
@@ -1,13 +1,9 @@
-emails=[]
-i=2
-while i<=50 && i%2==0 do
-  emails.push("jean.dupont.0#{i}@gmail.fr")
-   i = i + 2
-end
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étage veux tu?"
-etages=gets.to_i
-i=1
-while i <= etages do
-  puts "#"*i
-  i = i + 1
+puts "nombre d'étage?"
+etage=gets.to_i
+i= etage - 1
+j= 1
+while 0 <= etage do
+ puts "\s"*i + "#"*j
+i = i - 1
+j = j + 1
 end
